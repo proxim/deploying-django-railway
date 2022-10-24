24e84cb6721c28e7d7d5af63301014ee0f66e57f
proxim <61657192+proxim@users.noreply.github.com>
2022-10-23 19:23:37 -0500

    prep for deployment + add static files

--- deploythis/settings.py
+++ deploythis/settings.py
@@ -31,9 +31,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 ENV_VAR = os.getenv('ENV_VAR')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv('DEBUG') else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
